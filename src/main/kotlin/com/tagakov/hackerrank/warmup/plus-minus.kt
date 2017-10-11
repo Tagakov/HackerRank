348f5cccc2562506b1202579563aaec2f701c901
Vladimir Tagakov <vladimir@tagakov.com>
2017-10-12 00:52:48 +0300

    Solution of plus-minus problem

---
+++ src/main/kotlin/com/tagakov/hackerrank/warmup/plus-minus.kt
@@ -0,0 +1,21 @@
+package com.tagakov.hackerrank.warmup
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    val data = Scanner(System.`in`).readData()
+    var positive = 0
+    var negative = 0
+    for (i in data) {
+        if (i > 0) {
+            positive++
+        } else if (i < 0) {
+            negative++
+        }
+    }
+    println(positive.toFloat() / data.size)
+    println(negative.toFloat() / data.size)
+    println((data.size - positive - negative).toFloat() / data.size)
+}
+
+private fun Scanner.readData() = (1..nextInt()).map { nextInt() }
