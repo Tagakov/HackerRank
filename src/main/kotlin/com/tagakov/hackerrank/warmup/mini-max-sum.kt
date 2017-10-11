a09a2d9152257f504283d491a0ae627cd4f0dba9
Vladimir Tagakov <vladimir@tagakov.com>
2017-10-12 01:22:58 +0300

    Solution of mini-max-sum problem

---
+++ src/main/kotlin/com/tagakov/hackerrank/warmup/mini-max-sum.kt
@@ -0,0 +1,15 @@
+package com.tagakov.hackerrank.warmup
+
+import java.math.BigDecimal
+import java.util.*
+
+fun main(args: Array<String>) {
+    val list = Scanner(System.`in`).readList()
+    val min = list.dropLast(1).reduce(BigDecimal::add)
+    val max = list.drop(1).reduce(BigDecimal::add)
+    println("$min $max")
+}
+
+private fun Scanner.readList() = (1..5)
+        .map { nextBigDecimal() }
+        .sorted()
