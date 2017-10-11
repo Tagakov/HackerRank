4e3feb77b426be76a0821d324ee1e5b58a2fc6fa
Vladimir Tagakov <vladimir@tagakov.com>
2017-10-12 01:37:50 +0300

    solution of solve-me-first problem

---
+++ src/main/kotlin/com/tagakov/hackerrank/warmup/solve-me-first.kt
@@ -0,0 +1,13 @@
+package com.tagakov.hackerrank.warmup
+
+import java.util.*
+
+fun main(args: Array<String>) {
+    fun solveMeFirst(a: Int, b: Int) = a + b
+
+    val sc = Scanner(System.`in`)
+    val num1 = sc.nextInt()
+    val num2 = sc.nextInt()
+    val sum = solveMeFirst(num1, num2)
+    println(sum)
+}
