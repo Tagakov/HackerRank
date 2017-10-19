b51a1d63d40023c9e092e5074fc5f2d62333f74a
Vladimir Tagakov <vladimir@tagakov.com>
2017-10-19 13:04:44 +0300

    refactored solution of reverse-shuffle-merge problem

--- src/main/kotlin/com/tagakov/hackerrank/greedy/reverse-shuffle-merge.kt
+++ src/main/kotlin/com/tagakov/hackerrank/greedy/reverse-shuffle-merge.kt
@@ -1,73 +1,78 @@
 package com.tagakov.hackerrank.greedy
 
 import java.util.*
-import kotlin.collections.ArrayList
+import kotlin.collections.HashMap
 
-fun main(args: Array<String>) {
-    fun HashMap<Char, Int>.increment(c: Char) = ((get(c) ?: 0) + 1).also { put(c, it) }
-    fun HashMap<Char, Int>.decrement(c: Char) = ((get(c) ?: 0) - 1).also { put(c, it) }
+private class LineRegistry(line: String) {
+    inner class CharEntry(val index: Int) : Comparable<CharEntry> {
+        val char: Char = reversedLine[index]
+        val isMinimal = !sortedChars.isEmpty() && sortedChars.first() == char
+        val isCritical = remainingInResult[char] == charsCounter[char]
+        val isNeeded = remainingInResult[char] != 0
 
-    Scanner(System.`in`).apply {
-        val line = nextLine()
-
-        val charArray = line.toCharArray().reversedArray()
-        val resultStringSize = charArray.size / 2
-        val totals = hashMapOf<Char, Int>()
-
-        for (c in charArray) {
-            totals.increment(c)
-        }
-
-        val counts = HashMap(totals).apply { replaceAll { _, counter -> counter / 2 } }
-
-        val sorted = totals.keys.sorted().toMutableList()
-        val result = ArrayList<Char>(resultStringSize)
-
-        var i = 0
-        var smallestIndex = -1
-
-        while (i < charArray.size) {
-            val c = charArray[i++]
-            if (counts[c] == 0) continue
-            if (sorted[0] == c) {
-                result.add(c)
-                if (counts.decrement(c) == 0) {
-                    sorted.removeAt(0)
-                }
-                smallestIndex = -1
-            } else if (counts[c] == totals[c]) {
-                if (smallestIndex != -1 && c >= charArray[smallestIndex]) {
-                    val smallest = charArray[smallestIndex]
-                    result.add(smallest)
-                    if (counts.decrement(smallest) == 0) {
-                        sorted.remove(smallest)
-                    }
-                    while (i > smallestIndex + 1) {
-                        totals.increment(charArray[--i])
-                    }
-
-                } else {
-                    result.add(c)
-                    if (counts.decrement(c) == 0) {
-                        sorted.remove(c)
-                    }
-                }
-                smallestIndex = -1
-            } else {
-                val newSmallestIndex = i - 1
-                if (smallestIndex == -1) {
-                    smallestIndex = newSmallestIndex
-                } else {
-                    val smallestC = charArray[smallestIndex]
-                    if (smallestC > c) {
-                        smallestIndex = newSmallestIndex
-                    }
-                }
-            }
-            totals.decrement(c)
-        }
-
-        println(result.joinToString(separator = ""))
+        override fun compareTo(other: CharEntry) = char.compareTo(other.char)
     }
 
+    private fun HashMap<Char, Int>.increment(c: Char) = ((get(c) ?: 0) + 1).also { put(c, it) }
+    private fun HashMap<Char, Int>.decrement(c: Char) = ((get(c) ?: 0) - 1).also { put(c, it) }
+
+    private val reversedLine = line.reversed()
+    private val charsCounter = line.fold(hashMapOf<Char, Int>()) { acc, c -> acc.apply { increment(c) } }
+    private val sortedChars = line.toSortedSet()
+    private val remainingInResult = HashMap(charsCounter).apply { replaceAll { _, counter -> counter / 2 } }
+
+    private var index = 0
+
+    val result = StringBuilder()
+
+    fun hasNext() = index < reversedLine.length
+
+    fun next() = CharEntry(index++).also { charsCounter.decrement(it.char) }
+
+    fun appendToResult(entry: CharEntry) {
+        if (remainingInResult.decrement(entry.char) == 0) {
+            sortedChars.remove(entry.char)
+        }
+        result.append(entry.char)
+    }
+
+    fun rewindToEntry(entry: CharEntry) {
+        for (i in entry.index + 1 until index) {
+            charsCounter.increment(reversedLine[i])
+        }
+        index = entry.index + 1
+    }
+}
+
+fun main(args: Array<String>) {
+    val lineRegistry = LineRegistry(Scanner(System.`in`).nextLine())
+
+    var minimalSkippedEntry: LineRegistry.CharEntry? = null
+
+    while (lineRegistry.hasNext()) {
+        val letter = lineRegistry.next()
+        when {
+            !letter.isNeeded -> {
+            }
+
+            letter.isMinimal -> {
+                lineRegistry.appendToResult(letter)
+                minimalSkippedEntry = null
+            }
+
+            letter.isCritical && minimalSkippedEntry != null && minimalSkippedEntry <= letter -> {
+                lineRegistry.rewindToEntry(minimalSkippedEntry)
+                lineRegistry.appendToResult(minimalSkippedEntry)
+                minimalSkippedEntry = null
+            }
+
+            letter.isCritical -> {
+                lineRegistry.appendToResult(letter)
+                minimalSkippedEntry = null
+            }
+
+            minimalSkippedEntry == null || minimalSkippedEntry > letter -> minimalSkippedEntry = letter
+        }
+    }
+    println(lineRegistry.result)
 }
